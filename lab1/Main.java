e7fb7f00073cbed2d7a6061812a66fafb4e38093
Pytko Dmytro <dmutro0793@gmail.com>
2018-06-10 16:21:59 +0300

    fixed variable name

--- lab1/Main.java
+++ lab1/Main.java
@@ -4,19 +4,19 @@ public class Main {
 
     public static void main(String args[])
     {
-        Book DarkTower = new Book();
-        Book SherlockHolmes = new Book("SherlockHolmes", "detective",  468, 14);
-        Book Endersgame = new Book("Endersgame", "fanatasy",  357, 16, "English");
+        Book darkTower = new Book();
+        Book sherlockHolmes = new Book("SherlockHolmes", "detective",  468, 14);
+        Book endersgame = new Book("Endersgame", "fanatasy",  357, 16, "English");
 
-        System.out.println(DarkTower.toString());
-        System.out.println(SherlockHolmes.toString());
-        System.out.println(Endersgame.toString());
+        System.out.println(darkTower.toString());
+        System.out.println(sherlockHolmes.toString());
+        System.out.println(endersgame.toString());
 
-        DarkTower.printSum();
-        SherlockHolmes.printSum();
-        Endersgame.printSum();
+        darkTower.printSum();
+        sherlockHolmes.printSum();
+        endersgame.printSum();
 
-        SherlockHolmes.setNumberOfPages(525);
+        sherlockHolmes.setNumberOfPages(525);
 
         Book.printStaticSum();
 
